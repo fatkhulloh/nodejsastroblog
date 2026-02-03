324af69d49ba0c24aca9155f6e16d6ce4727644f
Fatkhulloh Hol <33220438+fatkhulloh@users.noreply.github.com>
2026-02-03 10:10:55 +0700

    fix icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-     devToolbar: {
+  devToolbar: {
     enabled: false,
   },
+  integrations: [
+    icon(), // ✨ integrasi astro-icon
+  ],
 });
